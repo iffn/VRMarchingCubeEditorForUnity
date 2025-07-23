5d8b34c639ad0b60b38d02d7e582572716afefa5
iffn <iffn1990@gmail.com>
2025-07-23 18:02:39 +0200

    Implemented scaling via keyboard

--- VRSimplePaint.cs
+++ VRSimplePaint.cs
@@ -11,10 +11,23 @@ public class VRSimplePaint : PlaymodeEditor
 {
     [SerializeField] private InputActionProperty editAction;
     [SerializeField] private InputActionProperty scaleActionOnY;
+    [SerializeField] CharacterController linkedCharacterController;
+
+    float defaultHeight;
+    float defaultRadius;
+    Vector3 defaultCenter;
 
     [SerializeField] float scaleSpeed = 1f;
     [SerializeField] float scaleThreshold = 0.01f;
     [SerializeField] Transform toolOrigin;
+
+    float CurrentScale
+    {
+        get
+        {
+            return linkedCharacterController.transform.localScale.x;
+        }
+    }
 
     void OnEnable()
     {
@@ -28,6 +41,8 @@ public class VRSimplePaint : PlaymodeEditor
     void Start()
     {
         InitializeController();
+
+        initializeVREditor();
     }
 
     void Update()
@@ -46,5 +61,36 @@ public class VRSimplePaint : PlaymodeEditor
         {
             placeableByClick.transform.localScale *= 1 + scaleValue * scaleSpeed * Time.deltaTime;
         }
+
+        if (Input.GetKey(KeyCode.KeypadPlus))
+        {
+            scalePlayer(CurrentScale * (1 + 0.25f * Time.deltaTime));
+            Debug.Log("Scale");
+        }
+
+        if (Input.GetKey(KeyCode.KeypadMinus))
+        {
+            scalePlayer(CurrentScale * (1 - 0.2f * Time.deltaTime));
+        }
+    }
+
+    void initializeVREditor()
+    {
+        defaultHeight = linkedCharacterController.height;
+        defaultRadius = linkedCharacterController.radius;
+        defaultCenter = linkedCharacterController.center;
+    }
+
+    void scalePlayer(float scale)
+    {
+        float scaleFactor = scale / CurrentScale; //Needs to be done before changing it
+
+        linkedCharacterController.transform.localScale = scale * Vector3.one;
+
+        linkedCharacterController.height = scale * defaultHeight;
+        linkedCharacterController.radius = scale * defaultRadius;
+        linkedCharacterController.center = scale * defaultCenter;
+
+        placeableByClick.transform.localScale *= scaleFactor;
     }
 }
